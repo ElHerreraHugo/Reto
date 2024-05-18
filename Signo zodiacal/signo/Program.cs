ebda7b484fa6907b17fa754d42dd5c09f28736f5
ElHerreraHugo <hugoherrera654@gmail.com>
2024-05-18 06:09:23 -0400

    commit 4

---
+++ Signo zodiacal/signo/Program.cs
@@ -0,0 +1,42 @@
+﻿
+        Console.WriteLine("Ingrese el día de su nacimiento:");
+        int dia = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el mes de su nacimiento:");
+        string mes = Console.ReadLine().ToLower(); 
+
+       
+        string zodiacSign = "";
+
+        if (mes == "marzo")
+        {
+            if (dia >= 21)
+            {
+                zodiacSign = "Aries";
+            }
+            else
+            {
+                zodiacSign = "Piscis";
+            }
+        }
+        else if (mes == "abril")
+        {
+            if (dia >= 20)
+            {
+                zodiacSign = "Tauro";
+            }
+            else
+            {
+                zodiacSign = "Aries";
+            }
+        }
+
+        if (zodiacSign != "")
+        {
+            Console.WriteLine("\nSu signo zodiacal es: " + zodiacSign);
+        }
+        else
+        {
+            Console.WriteLine("Fecha de nacimiento inválida.");
+        }
+    
