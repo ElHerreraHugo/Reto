1b2af165ff67ede9bf7164a6f8f1bae52ab15c99
ElHerreraHugo <hugoherrera654@gmail.com>
2024-05-18 06:08:27 -0400

    commit 3

---
+++ Área y perímetro/parimetro/Program.cs
@@ -0,0 +1,15 @@
+﻿//Pide al usuario las dimensiones de un rectángulo (base y altura) y calcula su área y perímetro.
+
+    
+        Console.WriteLine("Ingrese la base del rectángulo:");
+        double baseRectangulo = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese la altura del rectángulo:");
+        double alturaRectangulo = Convert.ToDouble(Console.ReadLine());
+
+        double area = baseRectangulo * alturaRectangulo;
+        double perimetro = 2 * (baseRectangulo + alturaRectangulo);
+
+        Console.WriteLine("El área del rectángulo es: " + area);
+        Console.WriteLine("El perímetro del rectángulo es: " + perimetro);
+    
